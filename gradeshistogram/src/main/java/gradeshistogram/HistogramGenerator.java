9f0b9d59c29e5cb18d30bc9157c757019992e136
Aliki-Ntouzgou <aliki.ntouzgou@gmail.com>
2020-04-07 19:05:56 +0300

    Merge the JFreeChartXYLineChartDemo class into the HistogramGenerator class

--- gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -1,29 +1,79 @@
 package gradeshistogram;
 
-import java.io.File;
-import java.util.ArrayList;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import java.util.Scanner;
-import java.io.FileNotFoundException; 
+import java.io.*;
 
 public class HistogramGenerator {
-	
-	public static void main(String[] args) throws FileNotFoundException {    
-		Scanner input = new Scanner(new File(args[200]));
-		ArrayList<String> grades = new ArrayList<String>();
-		while(input.hasNext()){
-    	   grades.add(input.next());
+
+	/***
+	 * Receives a single dimension Integer array. From this array the dataset
+	 * that will be used for the visualization is generated. Finally, The chart
+	 * is generated with the use of the aforementioned dataset and then
+	 * presented in the screen.
+	 *
+	 * @param dataValues Single dimension integer array
+	 */
+	public void generateChart(int[] dataValues) {
+		/*
+		 * The XYSeriesCollection object is a set XYSeries series (dataset) that
+		 * can be visualized in the same chart
+		 */
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		/*
+		 * The XYSeries that are loaded in the dataset. There might be many
+		 * series in one dataset.
+		 */
+		XYSeries data = new XYSeries("random values");
+
+		/*
+		 * Populating the XYSeries data object from the input Integer array
+		 * values.
+		 */
+		for (int i = 0; i < dataValues.length; i++) {
+			data.add(i, dataValues[i]);
 		}
-		JFreeChartXYLineChartDemo chart = new JFreeChartXYLineChartDemo();
+
+		// add the series to the dataset
+		dataset.addSeries(data);
+
+		boolean legend = false; // do not visualize a legend
+		boolean tooltips = false; // do not visualize tooltips
+		boolean urls = false; // do not visualize urls
+
+		// Declare and initialize a createXYLineChart JFreeChart
+		JFreeChart chart = ChartFactory.createXYLineChart("Chart title", "x_axis title", "y_axis_title", dataset,
+				PlotOrientation.VERTICAL, legend, tooltips, urls);
+
+		/*
+		 * Initialize a frame for visualizing the chart and attach the
+		 * previously created chart.
+		 */
+		ChartFrame frame = new ChartFrame("First", chart);
+		frame.pack();
+		// makes the previously created frame visible
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		Scanner s = new Scanner(System.in);
+		int[] grades = new int[149];
+
+		while (s.hasNextInt()) {
+			int count = s.nextInt();
+			if (count >= 0 && count <= 148) {
+				grades[count]++;
+			}
+		}
+
+		HistogramGenerator chart = new HistogramGenerator();
 		chart.generateChart(grades);
-	}//end main
-
-}//end class
-
-
-
-/*try {
-Scanner input = new Scanner(new File(args[2]));
-} catch (FileNotFoundException ex) {
-System.out.println("No File Found!");
-return;
-}*/
+		s.close();
+	}
+}
