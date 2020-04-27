3132c6f2b36bdd0eb3623ba3f95ae6bcb5e43b59
Aliki-Ntouzgou <aliki.ntouzgou@gmail.com>
2020-04-27 18:44:19 +0300

    Alternated the test coverage at ArrayOperationsTest

--- unittesting/src/test/java/math/ArrayOperationsTest.java
+++ unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -32,14 +32,22 @@ public class ArrayOperationsTest {
 	/*
 	 * Tests when the prime_integers array is empty and throws an exception
 	 */
-	@Test
-	public void test_primeintegers_Mocking() {
+	@Test (expected = IllegalArgumentException.class)
+	public void test_primeintegers_exception() {
+		//Mock the FileIO dependency
+		FileIO fio = mock(FileIO.class);
+		when(fio.readFile("filepath")).thenReturn(new int[] {2,4,6,8});
 		
 		//Mock the MyMath dependency
 		MyMath mym = mock(MyMath.class);
-		when(mym.isPrime(4)).thenThrow(IllegalArgumentException.class);
+		when(mym.isPrime(2)).thenReturn(false);
+		when(mym.isPrime(4)).thenReturn(false);
+		when(mym.isPrime(6)).thenReturn(false);
+		when(mym.isPrime(8)).thenReturn(false);
 		
+		arop.findPrimesInFile(fio, "filepath", mym);
 	}
+
 		
 }
 
